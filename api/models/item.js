4fc9c329cb20e360c8d74367dc0ec6feaec98a2d
testdeveloper34 <testdeveloper34@gmail.com>
2018-04-14 17:00:44 +0530

    added item model

---
+++ api/models/item.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    constantsObj = require('./../../constants');
+var Schema = mongoose.Schema;
+
+var ItemSchema = new mongoose.Schema({
+
+    item_name: {
+        type: String,
+        required: [true, 'Item name is required']
+    },
+    item_price: {
+        type: number,
+        required: [true, 'Price is required']
+    },
+    item_description: {
+        type: String,
+        required: [true, 'Description is required']
+    },
+    category_id: {
+        type: String,
+        require: [true, 'Password is required']
+    }
+}, {
+    timestamps: true
+});
+
+var Item = mongoose.model('Item', ItemSchema);
+module.exports = Item;
