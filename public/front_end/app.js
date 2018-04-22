8a09f40d2163a6454a1c3c23637da53641183ea0
Akshay Pant <pantakshay007@gmail.com>
2018-04-22 13:55:10 +0530

    don't know

--- public/front_end/app.js
+++ public/front_end/app.js
@@ -1,21 +1,40 @@
 "use strict";
 
-angular.module('kabi', ['ui.router','LocalStorageModule']);
+angular.module('kabi', ['ui.router','LocalStorageModule','ngStorage']);
 
-    angular.module("kabi").config(function ($stateProvider, $urlRouterProvider, $locationProvider, $sceProvider) {
+    angular.module("kabi").config(function ($stateProvider, $urlRouterProvider, $locationProvider, $sceProvider,$httpProvider) {
         // $sceProvider.enabled(false);
+        // $httpProvider.interceptors.push(function ($q, $location, $localStorage) {
+        //     return {
+        //         request: function (config) {
+        //             config.headers = config.headers || {};
+        //             // config.headers['Authorization'] = 'Basic d2VudHdvcnRobWFuOkNoYW5nZV9tZQ==';
+        //             config.headers['authorization'] = 'admin_bearer ' + $localStorage.token;
+        //             config.headers['client-type'] = 'browser'; // this is used to detect the request is from the browser
+        //             return config;
+        //         },
+        //         response: function (response) {
+        //             if (response.data.code == 401) {
+        //                 delete $localStorage.token;
+        //                 // handle the case where the user is not authenticated
+        //                 $location.path('/login');
+        //             }
+        //             return response || $q.when(response);
+        //         }
+        //     };
+        // });
         //$locationProvider.hashPrefix('');
-        $urlRouterProvider.when('', '/');
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.when('', '/test');
+        $urlRouterProvider.otherwise('/test');
         $stateProvider
             .state('Dashboard.Home', {
-                url: '/',
+                url: '/test',
                 templateUrl:  '/spa/modules/home/home.html',
                 controller:'homeCtrl'
             })
          .state('Dashboard.Test', {
                 url: '/',
-                templateUrl:  '/spa/modules/home/test.html',
+                templateUrl:  '/spa/modules/product/addupdateCategory.html',
                 // controller:'homeCtrl'
             })
             .state('Dashboard', {
--- public/front_end/spa/modules/home/homeCtrl.js
+++ public/front_end/spa/modules/home/homeCtrl.js
@@ -1,4 +1,3 @@
  "use strict";   
 angular.module('kabi').controller('homeCtrl', ['$scope','$http',function ($scope,$http) {
-    alert('home');
     }]);
